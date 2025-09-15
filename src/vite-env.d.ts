50d449cfb30b1cb3fb32c274dc8ee4d9f255bede
Trung Nguyen <trung1704ptit@gmail.com>
2025-09-15 13:54:43 +0700

    UPDATE: vite config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,8 +6,6 @@ interface ImportMetaEnv {
   readonly VITE_THEME_ACCENT_COLOR: string;
   readonly VITE_THEME_SIDEBAR_LAYOUT: string;
   readonly VITE_SHOW_BREADCRUMB: string;
-
-  // more env variables...
 }
 
 interface ImportMeta {
